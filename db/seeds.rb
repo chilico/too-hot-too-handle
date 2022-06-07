043898dff7b12f5856d6dfaf1d05515585e7346e
chris owen <farawaychris.owen@gmail.com>
2022-06-07 17:48:10 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,66 @@
 require_relative 'seeds_areas'
 require_relative 'seeds_images'
 
-puts Creating Chris Maryam and Estef users ...
+puts 'Creating Chris, Maryam and Estef users ...'
 chris = User.create(
-  first_name: 'Chris'
-  last_name: 'Owen'
-  address: 'Crouch End London UK'
-  email: 'chris@email.com'
-  password: '123456'
+  first_name: 'Chris',
+  last_name: 'Owen',
+  address: 'Crouch End, London, UK',
+  email: 'chris@email.com',
+  password: '123456',
   password_confirmation: '123456'
 )
-puts Created user: #{chris.first_name} #{chris.last_name}
+puts "Created user: #{chris.first_name} #{chris.last_name}"
 
 maryam = User.create(
-  first_name: 'Maryam'
-  last_name: 'Fazlali'
-  address: 'Wembley Park London UK'
-  email: 'maryam@email.com'
-  password: '123456'
+  first_name: 'Maryam',
+  last_name: 'Fazlali',
+  address: 'Wembley Park, London, UK',
+  email: 'maryam@email.com',
+  password: '123456',
   password_confirmation: '123456'
 )
-puts Created user: #{maryam.first_name} #{maryam.last_name}
+puts "Created user: #{maryam.first_name} #{maryam.last_name}"
 
 estef = User.create(
-  first_name: 'Estef'
-  last_name: 'Perez'
-  address: 'Clapham North London UK'
-  email: 'estef@email.com'
-  password: '123456'
+  first_name: 'Estef',
+  last_name: 'Perez',
+  address: 'Clapham North, London, UK',
+  email: 'estef@email.com',
+  password: '123456',
   password_confirmation: '123456'
 )
-puts Created user: #{estef.first_name} #{estef.last_name}
+puts "Created users: #{estef.first_name} #{estef.last_name}"
 
-puts Creating 5 random users ...
+puts 'Creating 5 random users ...'
 8.times do
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
-  area = ['london' 'cities'].sample
+  area = ['london', 'cities'].sample
   if area == 'london'
-    user.address = "#{@AREAS[area.to_sym].sample} London UK"
+    user.address = "#{@AREAS[area.to_sym].sample}, London, UK"
   else
-    user.address = "#{@AREAS[area.to_sym].sample} UK"
+    user.address = "#{@AREAS[area.to_sym].sample}, UK"
   end
-  user.phone_numer = Faker::PhoneNumber.cell_phone_in_e164
+  user.phone_number = Faker::PhoneNumber.cell_phone_in_e164
   user.email = Faker::Internet.email
   user.password = '123456'
   user.password_confirmation = '123456'
   user.save!
-  puts Created user: #{user.first_name} #{user.last_name}
+  puts "Created user: #{user.first_name} #{user.last_name}"
 end
 
 users = User.all
 
 # resources
-SPECIES = {annuum: %w[bell cayenne jalapeno poblano serrano]
-  baccatum: %w[aji]
-  chinense: %w[carolina_reaper habenero naga scotch_bonnet]
-  frutescens: %w[piri_piri tabasco]
-  pubescens: %w[manzano rocoto]}
+SPECIES = {
+  annuum: %w[bell cayenne jalapeno poblano serrano],
+  baccatum: %w[aji],
+  chinense: %w[carolina_reaper habenero naga scotch_bonnet],
+  frutescens: %w[piri_piri tabasco],
+  pubescens: %w[manzano rocoto]
+}
 
 SPICY = %w[
   spicy
@@ -94,17 +96,17 @@ users.sample(3).each do |user|
     chilli.variety = "#{SPICY.sample.capitalize} #{variety.capitalize} Seeds"
     chilli.species = species
     chilli.heat = HEAT.keys.sample.to_s
-    chilli.shu = HEAT[chilli.heat.to_sym]
+    chilli.shu = "#{HEAT[chilli.heat.to_sym].min}-#{HEAT[chilli.heat.to_sym].max + 1}"
     chilli.quantity = rand(1..100)
-    chilli.description = Faker::Hipster.sentences(number: 4).join("#{SPICY.sample}. ")
+    chilli.description = Faker::Hipster.sentences(number: 4).join(" #{SPICY.sample.capitalize}. ")
     chilli.chilli_type = 'seeds'
     chilli.date_available = [Date.today, (Date.today + rand(10..70))].sample
-    chilli.price = number_with_precision(rand(0.0..75.0).round(2), precision: 2)
+    chilli.price = rand(0.0..75.0).round(2)
 
     @IMAGES[:seeds].sample do |url|
       photo = URI.open(url)
       filename = "#{chilli.variety.downcase.gsub(' ', '_')}.jpg"
-      chilli.photos.attach(io: photo filename: filename content_type: image/png)
+      chilli.photos.attach(io: photo, filename: filename, content_type: 'image/png')
     end
     chilli.save
   end
@@ -121,20 +123,20 @@ users.sample(3).each do |user|
     chilli.variety = "Dried #{SPICY.sample.capitalize} #{variety.capitalize}"
     chilli.species = species
     chilli.heat = HEAT.keys.sample.to_s
-    chilli.shu = HEAT[chilli.heat.to_sym]
+    chilli.shu = "#{HEAT[chilli.heat.to_sym].min}-#{HEAT[chilli.heat.to_sym].max + 1}"
     chilli.quantity = rand(1..100)
-    chilli.description = Faker::Hipster.sentences(number: 4).join("#{SPICY.sample}. ")
+    chilli.description = Faker::Hipster.sentences(number: 4).join(" #{SPICY.sample.capitalize}. ")
     chilli.chilli_type = 'dried'
     chilli.date_available = [Date.today, (Date.today + rand(10..70))].sample
-    chilli.price = number_with_precision(rand(0.0..75.0).round(2), precision: 2)
+    chilli.price = rand(0.0..75.0).round(2)
 
     @IMAGES[:dried].sample do |url|
       photo = URI.open(url)
       filename = "#{chilli.variety.downcase.gsub(' ', '_')}.jpg"
-      chilli.photos.attach(io: photo filename: filename content_type: image/png)
+      chilli.photos.attach(io: photo, filename: filename, content_type: 'image/png')
     end
     chilli.save
   end
 end
 
-puts Successfully seeded database!
+puts 'Successfully seeded database!'
