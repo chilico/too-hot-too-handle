b71b7215ae9a578ba2197ecd3716bcf42339bbb5
chris owen <farawaychris.owen@gmail.com>
2022-06-07 11:17:00 +0100

    added all models and thier controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_163427) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_094851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_163427) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "chillis", force: :cascade do |t|
     t.string "variety"
@@ -57,6 +58,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_163427) do
     t.index ["user_id"], name: "index_chillis_on_user_id"
   end
 
+  create_table "sales", force: :cascade do |t|
+    t.string "status"
+    t.string "shipping_method"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sales_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +82,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_163427) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chillis", "users"
+  add_foreign_key "sales", "users"
 end
