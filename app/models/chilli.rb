c3917f6a63923c3c009866f385a5be3f59244cc8
Maryam Fazlali <fazlali1202@gmail.com>
2022-06-14 13:07:26 +0100

    Implemented search by heat section

--- app/models/chilli.rb
+++ app/models/chilli.rb
@@ -1,16 +1,15 @@
 class Chilli < ApplicationRecord
+  include AlgoliaSearch
   belongs_to :user
   has_many :sale_chillis
   has_many :flags
   has_many_attached :photos
   monetize :price_cents
 
-  include AlgoliaSearch
-
   algoliasearch do
     # all attributes will be sent
-    attributes :variety, :chilli_type, :species, :description, :heat
+    attributes :variety, :chilli_type, :species, :heat, :description
 
-    searchableAttributes ['chilli_type', 'species', 'heat', 'variety']
+    searchableAttributes ['unordered(chilli_type)', 'unordered(heat)', 'unordered(species)', 'unordered(variety)']
   end
 end
