9e91b5de568d7c8e842789e176c820d3b8adf777
Maryam Fazlali <fazlali1202@gmail.com>
2022-06-14 17:05:04 +0100

    Impelmented basic chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
 
   patch '/sales/:id', to: 'sales#update', as: :update_basket
   get '/sales/:id/confirm', to: 'sales#confirm', as: :order_confirmation
+
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
